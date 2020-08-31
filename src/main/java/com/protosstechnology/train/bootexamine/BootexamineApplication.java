5ea8e579f413527e2b2325f2113bca7fc9522d2d
protoss-academy <70457071+protoss-academy@users.noreply.github.com>
2020-08-31 14:24:05 +0700

    test push

--- src/main/java/com/protosstechnology/train/bootexamine/BootexamineApplication.java
+++ src/main/java/com/protosstechnology/train/bootexamine/BootexamineApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootexamineApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootexamineApplication.class, args);
+		SpringApplication.run(BootexamineApplication.class, args); 
 	}
 
 }
